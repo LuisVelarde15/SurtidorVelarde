1a6499b501456711aabc3c5b39f72cdfafd6df3f
Luis Velarde <*robertonu1504@gmail.com*>
2021-07-15 20:09:42 -0600

    Actualizacion

---
+++ APIWebVelarde/Models/Venta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIWeb.Models
+{
+    public class Venta
+    {
+
+        public int id { get; set; }
+        public int idcliente { get; set; }
+        public int idusuario { get; set; }
+        public int num_factura { get; set; }
+        public DateTime fecha_hora { get; set; }
+        public decimal impuesto { get; set; }
+        public decimal total { get; set; }
+        public ICollection <Venta_Detalle> detalles { get; set; }
+        public Venta()
+        {
+        }
+    }
+}
---
+++ APIWebVelarde/Models/Venta_Detalle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIWeb.Models
+{
+    public class Venta_Detalle
+    {
+        public int id { get; set; }
+        [ForeignKey("venta")]
+
+        public int idventa { get; set; }
+        public int idproducto { get; set; }
+        public int cantidad { get; set; }
+        public decimal precio { get; set; }
+        public decimal descuento { get; set; }
+        public Venta venta  { get; set; }
+
+
+        public Venta_Detalle()
+        {
+        }
+    }
+}

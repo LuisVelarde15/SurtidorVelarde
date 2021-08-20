b7d7ed627e56e5eb94e01e8da6c1eab38839376e
Luis_velarde15@outlook.com <Luis_velarde15@outlook.com>
2021-08-20 14:42:41 -0600

    Actializacion de codigo

--- APIWebVelarde/Controllers/CategoriasController.cs
+++ APIWebVelarde/Controllers/CategoriasController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
+//Nuñez Velarde Luis Roberto
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace APIWebVelarde.Controllers
